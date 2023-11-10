a144e747c93847a3acede8de314e80ce8a27a724
RaphMt <rafa.mtorres117@outlook.com>
2023-11-10 01:05:47 -0300

    .

--- debxp/scripts/especiais.sh
+++ debxp/scripts/especiais.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+echo "PID da seção do shell do script atual: $$ "
+# $$ - variavel que armazena PID da seção atual
 
+echo "Status da execução do ultimo comando: $?"
+# $? - retorna 0 em caso de sucesso do ultimo comando
+
+# [[ teste ]]
+# trabalhar com status de saída: (0 = sucesso), ou (!0 = erro)
+
+[[ $? -eq 0 ]] && echo "sucesso" || echo "erro" 
+exit 0
